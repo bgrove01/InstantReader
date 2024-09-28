98a4acb84965d85d6e48b62b505a1e31bc0e991a
Bryan Grove <bryan@bgrove.io>
2024-09-28 10:52:16 -0400

    Initial Commit

---
+++ InstantReader/InstantReaderApp.swift
@@ -0,0 +1,32 @@
+//
+//  InstantReaderApp.swift
+//  InstantReader
+//
+//  Created by Bryan Grove on 2024-09-28.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct InstantReaderApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ InstantReaderTests/InstantReaderTests.swift
@@ -0,0 +1,16 @@
+//
+//  InstantReaderTests.swift
+//  InstantReaderTests
+//
+//  Created by Bryan Grove on 2024-09-28.
+//
+
+import Testing
+
+struct InstantReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
